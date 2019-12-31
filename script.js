776d92d97201ee39295dbb2ea7d6588730e9e1e2
Esther Walker <equestriennesrule@yahoo.com>
2019-12-30 20:20:02 -0800

    added buttons to javascript file and linked it to html file

---
+++ script.js
@@ -0,0 +1,10 @@
+var button9am = document.querySelector('#button9am');
+var button10am = document.querySelector('#button10am');
+var button11am = document.querySelector('#button11am');
+var button12pm = document.querySelector('#button12pm');
+var button1pm = document.querySelector('#button1pm');
+var button2pm = document.querySelector('#button2pm');
+var button3pm = document.querySelector('#button3pm');
+var button4pm = document.querySelector('#button4pm');
+var button5pm = document.querySelector('#button5pm');
+
