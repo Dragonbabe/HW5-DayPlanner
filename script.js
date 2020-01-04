27020e8335653aeaaa66e4ec0113180dfc737b37
Esther Walker <equestriennesrule@yahoo.com>
2020-01-03 18:13:35 -0800

    Add functionality for color slots

--- script.js
+++ script.js
@@ -9,68 +9,83 @@ var button3pm = document.querySelector('#button3pm');
 var button4pm = document.querySelector('#button4pm');
 var button5pm = document.querySelector('#button5pm');
 var input9amElement = document.querySelector('#input9am');
-var currentTime = moment().format('HH'); 
-if (9 < currentTime){
-    input9amElement.style.backgroundColor = 'grey';
-}
-(function()
-{
-//Put in a moment object
-var momentObject = moment();
+var input10amElement = document.querySelector('#input10am');
+var input11amElement = document.querySelector('#input11am');
+var input12pmElement = document.querySelector('#input12pm');
+var input1pmElement = document.querySelector('#input1pm');
+var input2pmElement = document.querySelector('#input2pm');
+var input3pmElement = document.querySelector('#input3pm');
+var input4pmElement = document.querySelector('#input4pm');
+var input5pmElement = document.querySelector('#input5pm');
+var inputArray = document.getElementsByTagName('input');
 
-// display value of moment object referenced in html in the displayMoment div
-var displayMoment = document.getElementById('displayMoment');
-displayMoment.innerHTML = momentObject.format('YYYY-M-D');
+var currentTime = parseInt(moment().format('HH'));
+for (var i = 0; i < inputArray.length; i++) {
+
+    var currentDataTime = parseInt(inputArray[i].getAttribute('data-time'));
+    console.log(currentDataTime, currentTime, inputArray[i]);
+    if (currentDataTime < currentTime) {
+        inputArray[i].style.backgroundColor = 'grey';
+    }
+
+    if (currentDataTime === currentTime) {
+        inputArray[i].style.backgroundColor = 'green';
+    }
+    if (currentDataTime > currentTime) {
+        inputArray[i].style.backgroundColor = 'red';  
+    }
+
+}
+
+
+(function () {
+    //Put in a moment object
+    var momentObject = moment();
+
+    // display value of moment object referenced in html in the displayMoment div
+    var displayMoment = document.getElementById('displayMoment');
+    displayMoment.innerHTML = momentObject.format('YYYY-M-D');
 })();
 
 
 // add event listeners for each button to save values from the input into local storage
-button9am.addEventListener('click', function() {
-    
+button9am.addEventListener('click', function () {
     localStorage.setItem('appt9am', input9amElement.value);
 })
 
-button10am.addEventListener('click', function() {
-    var input10amValue = document.querySelector('#input10am').value;
-    localStorage.setItem('appt10am', input10amValue);
+button10am.addEventListener('click', function () {
+    localStorage.setItem('appt10am', input10amElement.value);
 })
 
-button11am.addEventListener('click', function() {
-    var input11amValue = document.querySelector('#input11am').value;
-    localStorage.setItem('appt11am', input11amValue);
+button11am.addEventListener('click', function () {
+    localStorage.setItem('appt11am', input11amElement.value);
 })
 
-button12pm.addEventListener('click', function() {
-    var input12pmValue = document.querySelector('#input12pm').value;
-    localStorage.setItem('appt12pm', input12pmValue);
+button12pm.addEventListener('click', function () {
+    localStorage.setItem('appt12pm', input12pmElement.value);
 })
 
-button1pm.addEventListener('click', function() {
-    var input1pmValue = document.querySelector('#input1pm').value;
-    localStorage.setItem('appt1pm', input1pmValue);
+button1pm.addEventListener('click', function () {
+    localStorage.setItem('appt1pm', input1pmElement.value);
 })
 
-button2pm.addEventListener('click', function() {
-    var input2pmValue = document.querySelector('#input2pm').value;
-    localStorage.setItem('appt2pm', input2pmValue);
+button2pm.addEventListener('click', function () {
+    localStorage.setItem('appt2pm', input2pmElement.value);
 })
 
-button3pm.addEventListener('click', function() {
-    var input3pmValue = document.querySelector('#input3pm').value;
-    localStorage.setItem('appt3pm', input3pmValue);
+button3pm.addEventListener('click', function () {
+    localStorage.setItem('appt3pm', input3pmElement.value);
 })
 
-button4pm.addEventListener('click', function() {
-    var input4pmValue = document.querySelector('#input4pm').value;
-    localStorage.setItem('appt4pm', input4pmValue);
+button4pm.addEventListener('click', function () {
+    localStorage.setItem('appt4pm', input4pmElement.value);
 })
 
-button5pm.addEventListener('click', function() {
-    var input5pmValue = document.querySelector('#input5pm').value;
-    localStorage.setItem('appt5pm', input5pmValue);
+button5pm.addEventListener('click', function () {
+    localStorage.setItem('appt5pm', input5pmElement.value);
 })
 
-window.onload = function(){
+window.onload = function () {
     // get the values in local storage back in the input boxes where the users type stuff when the page is refreshed
     document.querySelector('#input9am').value = localStorage.getItem('appt9am');
     document.querySelector('#input10am').value = localStorage.getItem('appt10am');
